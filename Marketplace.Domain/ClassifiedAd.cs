96df4299c9ac5d646b3bbdfbe96680ee55d355d3
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-05-06 22:40:22 -0500

    Classified ad entity

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+
+        public ClassifiedAd(Guid id, Guid ownerId)
+        {
+            if(id == default) throw new ArgumentException("Identity must be specified", nameof(id));
+            Id = id;
+            _ownerId = ownerId;
+        }
+
+        public void SetTitle(string title) => _title = title;
+        public void UpdateText(string text) => _text = text;
+        public void UpdatePrice(decimal price) => _price = price;
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
