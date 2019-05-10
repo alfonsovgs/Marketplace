43386da5dcd9f876d00fca48a31d683296079013
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-05-09 21:58:56 -0500

    Contracts

---
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Api
+{
+    [Route("/ad")]
+    public class ClassifiedAdsCommandsApi : Controller
+    {
+        public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
+        {
+            //Handle the request here
+            return Ok();
+        }
+    }
+}
