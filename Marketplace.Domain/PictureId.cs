3a30998ddc4109736b48828bb42e02380ad0f48d
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-05-12 22:28:05 -0500

    Add new entities

---
+++ Marketplace.Domain/PictureId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Marketplace.Framework;
+
+namespace Marketplace.Domain
+{
+    public class PictureId : Value<PictureId>
+    {
+        public Guid Value { get; }
+
+        public PictureId(Guid value)
+        {
+            Value = value;
+        }
+    }
+}
